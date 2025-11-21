9c1d550b785f1cb5acf03f975a8e646917b16cc4
cydacode <nwacynti25@gmail.com>
2025-11-21 16:22:43 +0100

    Creating a full deployment pipeline

--- buildscript.sh
+++ buildscript.sh
@@ -3,8 +3,8 @@
 echo "Logging into Docker"
 # echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker build -t $DOCKER_USERNAME/wtf_repo:be_v1 ./backend/Dockerfile
-docker build -t $DOCKER_USERNAME/wtf_repo:fe_v1 ./dive-react-app/Dockerfile
+docker build -t $DOCKER_USERNAME/wtf_repo:be_v1 ./backend
+docker build -t $DOCKER_USERNAME/wtf_repo:fe_v1 ./dive-react-app/
 
 docker push $DOCKER_USERNAME/wtf_repo:be_v1
 docker push $DOCKER_USERNAME/wtf_repo:fe_v1
