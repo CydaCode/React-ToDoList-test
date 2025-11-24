04b88568bcbf1d11ba46d9cf4bc60d949cf21312
cydacode <nwacynti25@gmail.com>
2025-11-24 14:07:50 +0100

    Made changes to the Jenkinsfile.sh

--- buildscript.sh
+++ buildscript.sh
@@ -4,7 +4,7 @@ echo "Logging into Docker"
 # echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker build -t $DOCKER_USERNAME/wtf_repo:be_v1 -f backend/Dockerfile backend
-docker build -t $DOCKER_USERNAME/wtf_repo:fe_v1 -f dive-react-app/Dockerfile frdive-react-app
+docker build -t $DOCKER_USERNAME/wtf_repo:fe_v1 -f dive-react-app/Dockerfile dive-react-app
 
 docker push $DOCKER_USERNAME/wtf_repo:be_v1
 docker push $DOCKER_USERNAME/wtf_repo:fe_v1
