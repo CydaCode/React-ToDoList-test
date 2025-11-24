3e9633f840c2ed84ac0d5cf5a7bbea2c9c567813
cydacode <nwacynti25@gmail.com>
2025-11-24 10:21:23 +0100

    Made changes to the buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -3,8 +3,8 @@
 echo "Logging into Docker"
 # echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker build -t $DOCKER_USERNAME/wtf_repo:be_v1 ./backend
-docker build -t $DOCKER_USERNAME/wtf_repo:fe_v1 ./dive-react-app/
+docker build -t $DOCKER_USERNAME/wtf_repo:be_v1 -f backend/Dockerfile backend
+docker build -t $DOCKER_USERNAME/wtf_repo:fe_v1 -f dive-react-app/Dockerfile frdive-react-app
 
 docker push $DOCKER_USERNAME/wtf_repo:be_v1
 docker push $DOCKER_USERNAME/wtf_repo:fe_v1
